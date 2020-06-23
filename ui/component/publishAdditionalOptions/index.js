4004e9972771bb748773ce17fefa3fb8049b018f
Sean Yesmunt <sean@lbry.io>
2020-06-23 17:56:41 -0400

    Initial commit for LBRY-First
    moar
    
    Add check

--- ui/component/publishAdditionalOptions/index.js
+++ ui/component/publishAdditionalOptions/index.js
@@ -1,16 +1,18 @@
 import { connect } from 'react-redux';
 import { selectPublishFormValues, doUpdatePublishForm } from 'lbry-redux';
 import PublishPage from './view';
+import { doFetchAccessToken, selectAccessToken } from 'lbryinc';
+import { selectUser } from '../../redux/selectors/user';
 
 const select = state => ({
   ...selectPublishFormValues(state),
+  accessToken: selectAccessToken(state),
+  user: selectUser(state),
 });
 
 const perform = dispatch => ({
   updatePublishForm: value => dispatch(doUpdatePublishForm(value)),
+  fetchAccessToken: () => dispatch(doFetchAccessToken()),
 });
 
-export default connect(
-  select,
-  perform
-)(PublishPage);
+export default connect(select, perform)(PublishPage);
